cb5f9358ff4bb533a2cb5e22c6db3015ed765516
ohllamalow <92478950+ohllamalow@users.noreply.github.com>
2025-06-20 13:50:34 -0700

    PizzaPop background

--- Assets/MiniGames/Scripts/Pizza Pop/Bubble/BubbleSpawner.cs
+++ Assets/MiniGames/Scripts/Pizza Pop/Bubble/BubbleSpawner.cs
@@ -3,47 +3,61 @@ using System.Collections;
 
 public class BubbleSpawner : MonoBehaviour
 {
-    [SerializeField] GameObject _bubblePrefab;
-
+    [Header("Spawning")]
     [Space(5)]
+    [SerializeField] GameObject _bubblePrefab;
     [SerializeField] Transform _spawnCenter;
     [Space(5)]
-    [SerializeField] float _radius = 1f;
-    
+    [SerializeField] private float _radius = 1f;
+
+    [Header("Difficulty")]
+    [Space(5)]
+    private int _numberOfBubbles;
+    private int _minBubbles = 8;
+    private int _maxBubbles = 10;
+    private int _currentBubble = 0;
+
     void Start()
     {
+        _numberOfBubbles = Random.Range(_minBubbles, _maxBubbles);
         StartCoroutine(StartSpawningBubbles());
     }
 
     private IEnumerator StartSpawningBubbles()
     {
-        while (true)
+        while (_numberOfBubbles > _currentBubble)
         {
             SpawnBubble();
-            float nextSpawnTimer = Random.Range(0f, 2f);
+            float nextSpawnTimer = Random.Range(1f, 4f);
 
             yield return new WaitForSeconds(nextSpawnTimer);
-        };
+        }
+
+        Debug.Log("Game Over");
+        ;
     }
 
-    public void SpawnBubble()
-    {
-        Vector3 spawnPosition = RandomPointOnXZPlane();
-        Instantiate(_bubblePrefab, spawnPosition, Quaternion.identity);
-        Debug.Log("Spawning bubble");
-    }
-
-    private Vector3 RandomPointOnXZPlane()
-    {
-        Vector2 spawnPosition = RandomPointOnPizza();
-        Vector3 center = _spawnCenter.position;
-        return new Vector3(center.x + spawnPosition.x, center.y, center.z + spawnPosition.y);
-    }
-
-    private Vector2 RandomPointOnPizza()
-    {
-        float angle = Random.Range(0f, Mathf.PI * 2f);
-        float distance = Mathf.Sqrt(Random.Range(0f, 1f)) * _radius;
-        return new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * distance;
-    }
+#region SpawnBubble func's
+        public void SpawnBubble()
+        {
+            Vector3 spawnPosition = RandomPointOnXZPlane();
+            Instantiate(_bubblePrefab, spawnPosition, Quaternion.identity);
+    
+            _currentBubble++;
+        }
+    
+        private Vector3 RandomPointOnXZPlane()
+        {
+            Vector2 spawnPosition = RandomPointOnPizza();
+            Vector3 center = _spawnCenter.position;
+            return new Vector3(center.x + spawnPosition.x, center.y, center.z + spawnPosition.y);
+        }
+    
+        private Vector2 RandomPointOnPizza()
+        {
+            float angle = Random.Range(0f, Mathf.PI * 2f);
+            float distance = Mathf.Sqrt(Random.Range(0f, 1f)) * _radius;
+            return new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * distance;
+        }
+#endregion
 }
